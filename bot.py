465babbddd440df337c4838c61b66ab8823ed6a5
unknown <>
2017-04-03 22:33:18 +0900

    Initial commit

---
+++ bot.py
@@ -0,0 +1,9 @@
+#coding:utf-8
+
+import requests
+
+URL='https://slack.com/api/chat.postMessage'
+TOKEN='xoxb-156491807943-Zp2XO0yH9KGxO4wcR3IZc0hY'
+param={'token':TOKEN,'channel':'test','text':'testt'}
+
+requests.post(URL,params=param)
