43548856b5ac4b1c73dab5607fb8251e1fd7940d
tksy8 <taku28_kei13@yahoo.co.jp>
2017-04-04 19:12:05 +0900

    リプライ追加 - 天気の確認

--- plugins/reply.py
+++ plugins/reply.py
@@ -13,3 +13,23 @@ def welcome_back(message):
 @listen_to('おはよう')
 def good_morning(message):
     message.send('にゃあ')
+
+
+@listen_to('天気')
+@listen_to('晴れ')
+@listen_to('雨')
+@respond_to('天気')
+def weather(message):
+    import urllib
+    import json
+
+    url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city='
+    # '130010'とすると東京の情報を取得してくれる
+    # ここを変えれば任意の地域の天気情報を取得できる
+    city_id = '130010'
+
+    html = urllib.request.urlopen(url + city_id)
+    jsonfile = json.loads(html.read().decode('utf-8'))
+    text = jsonfile['description']['text']
+
+    message.send(text)
