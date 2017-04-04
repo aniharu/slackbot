9831d44dbec0d81a5dcf7cf8bad90c9b47aec045
tksy8 <taku28_kei13@yahoo.co.jp>
2017-04-04 18:39:15 +0900

    リプライ追加2訂正の訂正

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-API_TOKEN = "xoxb-156491807943-Zp2XO0yH9KGxO4wcR3IZc0hY"
+API_TOKEN = "xoxb-156491807943-54IF0PPyFEw2ptigqB7evHcu"
 default_reply = "スイマセン。其ノ言葉ワカリマセン"
 PLUGINS = [
     'plugins',
