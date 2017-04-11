8637543401403d1a6b461cfcfe46176fd399dec3
jackee777 <stardustchronicle10030709@gmail.com>
2017-04-12 08:36:52 +0900

    Update mouse_move_observation.py
    可読性がひどかったので、クラスにして向上。

--- mouse_move_observation.py
+++ mouse_move_observation.py
@@ -3,39 +3,11 @@ import ctypes
 import time
 import socket
 import errno
+import subprocess
 
 host = "your ip address"
 port = 8000
 #BUFF  = 1024
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-def try_connection():
-    while(True):
-        try:
-            client.connect((host, port))
-            break
-        except socket.error as exc:
-            print("Caught exception socket.error : %s" % exc)
-            #for reconnection error not to continue connecting
-            if exc.errno == errno.WSAEISCONN:#10056
-                break
-            continue
-
-#for reconnection error
-#client needs to wait time until socket deletes
-#and be created again ?
-#it is not stable
-#the concept of this method is remedy to restart a server
-
-#上手くいかないところ
-def reconnection(exc):
-    if exc.errno == errno.WSAECONNABORTED:#10053
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    elif exc.errno == errno.WSAECONNRESET:#10054
-        time.sleep(60)
-    try_connection()
-    weight = 0
 
 # define pointer class
 class _pointer(ctypes.Structure):
@@ -44,41 +16,75 @@ class _pointer(ctypes.Structure):
         ('y', ctypes.c_long),
     ]
 
-try_connection()
+class client(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.error = dict({ "ERROR_10053":"exc.errno == errno.WSAECONNABORTED",
+                            "ERROR_10054":"exc.errno == errno.WSAECONNRESET",
+                            "ERROR_10056":"exc.errno == errno.WSAEISCONN",
+                            "ERROR_10057":"exc.errno == errno.WSAENOTCONN" })
+        self.try_connection()
 
-# initialize class
-before_point = _pointer()
-after_point = _pointer()
-
-weight = -1
-state_on = "0"
-time_weight = [15, 15,15,15,15]#[15, 60, 180, 120, 180]
-
-while (1):
-    before_point.x = after_point.x
-    before_point.y = after_point.y
-    ctypes.windll.user32.GetCursorPos(ctypes.byref(after_point))
-
-    if int(before_point.x) == int(after_point.x) and int(before_point.y) == int(after_point.y):
-        weight = weight - 1 if weight >= 0 and state_on == "1" else 0
-    elif weight != 3:
-        weight += 1
-        if weight == 1 and state_on != "1":
-            state_on = "1"
+    def try_connection(self):
+        while(1):
             try:
-                client.send(state_on.encode("utf-8"))
+                self.client.connect((self.host, self.port))
+                break
             except socket.error as exc:
                 print("Caught exception socket.error : %s" % exc)
-                reconnection(exc)
+                #for reconnection error not to continue connecting
+                if self.error["ERROR_10056"] or self.error["ERROR_10057"]:
+                    break
                 continue
 
-    if weight == -1:
-        state_on = "0"
-        try:
-            client.send(state_on.encode("utf-8"))
-        except socket.error as exc:
-            print("Caught exception socket.error : %s" % exc)
-            reconnection(exc)
-            continue
+    def observe(self):
+        # initialize pointer
+        before_point = _pointer()
+        after_point = _pointer()
 
-    time.sleep(time_weight[weight + 1]/15)
+        weight = -1
+        state_on = 0
+        time_weight = [15, 60, 180, 120, 180]#待ち時間
+
+        while (1):
+            before_point.x = after_point.x
+            before_point.y = after_point.y
+            ctypes.windll.user32.GetCursorPos(ctypes.byref(after_point))
+
+            if before_point.x == after_point.x and before_point.y == after_point.y:
+                weight = weight - 1 if weight >= 0 and state_on else 0
+            elif weight != 3:
+                weight += 1
+                if weight == 1 and state_on != 1:
+                    state_on = 1
+                    try:
+                        self.client.send(str(state_on).encode("utf-8"))
+                    except socket.error as exc:
+                        print("Caught exception socket.error : %s" % exc)
+                        if self.error["ERROR_10053"] or self.error["ERROR_10054"]:
+                            break
+
+            if weight == -1:
+                state_on = 0
+                try:
+                    self.client.send(str(state_on).encode("utf-8"))
+                except socket.error as exc:
+                    print("Caught exception socket.error : %s" % exc)
+                    if self.error["ERROR_10053"] or self.error["ERROR_10054"]:
+                        break
+
+            time.sleep(time_weight[weight + 1])
+
+    def repeat(self):
+        # remedy to error
+        #本来はエラー対策だけでなく、サーバーが落ちた時の再アクセス用にループさせたかったのだが無理そう
+        #(サーバーが落ちてもsocketが残るため、プログラムを落とさないと、通信しなおせない)
+        #socketのclose, shutdown, timeoutなどは効果なしだった。
+        #クライアントの再起動をせずに済む案あれば。。。
+        while(1):
+            self.observe()
+
+if __name__ == "__main__":
+    client(host, port).repeat()
