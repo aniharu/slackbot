6b09a5acf53630ee7dea50a55ed66b8f97183c2c
jackee777 <stardustchronicle10030709@gmail.com>
2017-04-13 12:28:22 +0900

    Merge branch 'master' of C:\Users\yama\PycharmProjects\slackbot with conflicts.

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@
 import requests
 
 URL='https://slack.com/api/chat.postMessage'
-TOKEN='xoxb-156491807943-Zp2XO0yH9KGxO4wcR3IZc0hY'
+TOKEN='xoxb-156491807943-54IF0PPyFEw2ptigqB7evHcu'
 param={'token':TOKEN,'channel':'test','text':'testt'}
 
 requests.post(URL,params=param)
---
+++ slackbot_settings.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+API_TOKEN = "xoxb-156491807943-Zp2XO0yH9KGxO4wcR3IZc0hY"
+default_reply = "スイマセン。其ノ言葉ワカリマセン"
+PLUGINS = [
+    'plugins',
+]
